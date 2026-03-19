2f23d951cdef569e8baf239e264255ebe5fcc0be
altOpen <manash3sharma@gmail.com>
2026-03-19 15:53:13 +0530

    fix server syntax final

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
+// Routes
 const memberRoutes = require("./routes/members.routes");
-
 app.use("/api/members", memberRoutes);
 
+// Test route
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+// PORT (Render compatible)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
-});
 });
